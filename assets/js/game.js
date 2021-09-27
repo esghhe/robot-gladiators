6f50941b8afcf1e675cabbd931bcca078ec19329
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2021-09-27 15:23:39 -0400

    i added some variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has began!");
+    var playerName = window.prompt("What is your robot's name?");
+    // What is this?
+    console.log(playerName);
+    console.log("This logs a strings, good for leaving yourself a message")
+    //this will do much an log 20
+    console.log(10 + 10);
+    // what is this?
+    console.log("Our robot's name is " + playerName);
+    var playerName = "Tony the Robot";
+    // Tony the Robot is ready for battle!
+    console.log("Tony the Robot" + " is ready for battle!");
+    // "Tony the Robot is ready for battle!"
+    console.log(playerName + " is ready battle!");
+    // "Your robot, Tony the Robot, has won!"
+    console.log("Your robot, " + playerName + ", has won!");
+    
+}
+fight();
